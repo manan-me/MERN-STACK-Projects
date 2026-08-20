af31732fdcf784b80af8082a630c28d9cdbffaab
manan.me <abdulmanan30m@gmail.com>
2026-08-20 06:15:05 +0500

    atlas connection added

--- CRUD_SERVER/index.js
+++ CRUD_SERVER/index.js
@@ -1,64 +1,59 @@
 const express = require("express");
 const cors = require("cors");
-
+require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 
 //DB Connection
-const connectMongoDb=require('./dbConnection')
+const connectMongoDb = require("./dbConnection");
 
-connectMongoDb("mongodb://127.0.0.1:27017/crud")
-.then(()=>{console.log("Mongo DB is connected Succesfully");
-})
-.catch((e)=>{console.log(e);
-})
+connectMongoDb(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Mongo DB is connected Succesfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 app.use(cors());
 
-
 //Routes
-const user=require("./Model/crud")
+const user = require("./Model/crud");
 
-app.post('/create',(req,res)=>{
-    user.create({...req.body})
-    .then((user)=>{return res.json(user)})
-    .catch((e)=>{return res.json(e)})
-})
+app.post("/create", (req, res) => {
+  user
+    .create({ ...req.body })
+    .then((user) => {
+      return res.json(user);
+    })
+    .catch((e) => {
+      return res.json(e);
+    });
+});
 
-app.get('/',async  (req,res)=>{
-    const users=await user.find({})
-    console.log(users);
-    
-    return res.json(users)
-})
+app.get("/", async (req, res) => {
+  const users = await user.find({});
+  console.log(users);
 
-app.get('/getUser/:id', async (req,res)=>{
-    const id=req.params.id
-    const userr=await user.findById(id)
-    return res.json(userr)
-})
-app.post('/update/:id', async (req,res)=>{
-    const id=req.params.id
-    await user.findOneAndUpdate({_id:id},{...req.body})
-    return res.json({msg:'Done'})
-    
-})
+  return res.json(users);
+});
 
-app.delete('/delete/:id',async (req,res)=>{
-    const id=req.params.id
-    await user.findOneAndDelete({_id:id})
-    return res.json()
-})
+app.get("/getUser/:id", async (req, res) => {
+  const id = req.params.id;
+  const userr = await user.findById(id);
+  return res.json(userr);
+});
+app.post("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  await user.findOneAndUpdate({ _id: id }, { ...req.body });
+  return res.json({ msg: "Done" });
+});
 
-
-
-
-
-
-
-
-
-
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  await user.findOneAndDelete({ _id: id });
+  return res.json();
+});
 
 const port = 8000;
 
