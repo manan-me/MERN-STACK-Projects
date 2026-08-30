be355ce8ebdac86dd79ef32be9251140d0c1a54c
manan.me <abdulmanan30m@gmail.com>
2026-08-30 07:50:49 +0500

    Header section, with mobbbile scrreen compatibality

--- Real_Estate/client/src/App.jsx
+++ Real_Estate/client/src/App.jsx
@@ -4,9 +4,11 @@ import SignIn from "./pages/SignIn"
 import SignOut from "./pages/SignOut"
 import Profile from "./pages/Profile"
 import About from "./pages/about"
+import Header from "./Components/Header"
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
      <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/sign-in" element={<SignIn/>} />
---
+++ Real_Estate/client/src/Components/Header.jsx
@@ -0,0 +1,36 @@
+import { FaSearch } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto px-4 py-3">
+        
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Manan</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+
+        <form className="bg-slate-100 rounded-lg p-3 flex items-center gap-2">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none w-24 sm:w-64 text-sm"
+          />
+          <FaSearch className="text-slate-600 cursor-pointer" />
+        </form>
+
+        <ul className="flex gap-4 items-center text-slate-700 font-medium text-sm sm:text-base">
+          <Link to="/"><li className="hidden sm:inline hover:underline">Home</li></Link>
+          <Link to="/about"><li className="hidden sm:inline hover:underline">About</li></Link>
+          <Link to="/sign-in"><li className="hover:underline">Sign In</li></Link>
+        </ul>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
