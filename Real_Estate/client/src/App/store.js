3055310a33c3bddfc8f50289e575becd7a46d2cb
manan.me <abdulmanan30m@gmail.com>
2026-09-03 21:34:38 +0500

    Redux persist

--- Real_Estate/client/src/App/store.js
+++ Real_Estate/client/src/App/store.js
@@ -1,11 +1,30 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import userReducer from "../Features/userSlice"
-const Store=configureStore({
-    reducer:{user:userReducer},
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
-        serializableCheck:false
-    })
+import { persistReducer, persistStore } from "redux-persist"
 
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+}
 
+const rootReducer = combineReducers({
+  user: userReducer
 })
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const Store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false })
+})
+
+export const persistor = persistStore(Store)
 export default Store
